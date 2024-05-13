f961230d44c7d367c92a5cde3a0b5e1ab9b0269a
Francesco <sworld.design@gmail.com>
2024-05-13 15:42:26 +0200

    added new routes
    added the loggedIn route and added an home route without transition for the landing page

--- src/router.js
+++ src/router.js
@@ -1,12 +1,20 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 import HomePage from './presentation/components/HomePage.vue'
 import BookingPage from './presentation/components/BookingPage.vue'
+import LoggedIn from './presentation/components/LoggedIn.vue'
 
 const routes = [
     { 
+      name: "HomeDefault",
       path: '/', 
       component: HomePage,
-      meta: { transition: 'none' }
+    },
+
+    { 
+      name: "HomeAnimation",
+      path: '/', 
+      component: HomePage,
+      meta: { transition: 'home-in-top' }
     },
 
     {  
@@ -15,8 +23,15 @@ const routes = [
       component: BookingPage,  
       props: { BookingPage: true, numberOfUmbrella: 150 },
       meta: { transition: 'booking-in-bottom' }
-      
-    },  
+    },
+
+    {  
+      name: "LoggedIn",
+      path: '/logged', 
+      component: LoggedIn,  
+      meta: { transition: 'logged-in-right' }
+    },
+
 ]
 
 const router = createRouter({
