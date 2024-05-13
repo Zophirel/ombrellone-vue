7e1f41018b2b22aea521a70955400941d6ce54d3
Francesco <sworld.design@gmail.com>
2024-05-13 15:40:14 +0200

    new route component

---
+++ src/presentation/components/LoggedIn.vue
@@ -0,0 +1,50 @@
+<template> 
+    <div class="bg">
+        <div class="ctn"> 
+            <div class="tile">
+                <img class="icon" src="/src/assets/book.svg"/>
+                <p>Nuova prenotazione</p>
+            </div>
+            <div class="tile">
+                <img class="icon" src="/src/assets/booked.svg"/>
+                <p>Prenotazioni effettuate</p>
+            </div>
+            <div class="tile">
+                <img class="icon" src="/src/assets/account.svg"/>
+                <p>Account</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LoggedIn',        
+    }
+</script>
+
+<style scoped>
+
+    .bg{
+        background-color: aliceblue;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+    }
+
+    .ctn {
+        background-color: rgb(160, 160, 160);
+        border-radius: 5px;
+    }
+
+    .tile{
+        display: flex;
+        gap: 15px;
+        flex-direction: row;
+        padding: 10px;
+    }
+
+</style>
+  
