a125de8decb56f421e1300449a1d428d5a83146e
Francesco <sworld.design@gmail.com>
2024-05-21 16:40:26 +0200

    added reservation ending state sceen
    modified booking push to go back and replace, added success modal

--- src/presentation/components/reservation/ReservationModal.vue
+++ src/presentation/components/reservation/ReservationModal.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="modal-bg">
-        <div id="modal">
-            <div id="close" @click="this.$emit('closeModal')">x</div>
+        <div id="modal" v-show="!operationCompleated">
+            <div id="close" @click="closeModal">x</div>
             <div class="user-info">
                 <label>Nome e Congnome</label>
                 <input type="text" class="input" :placeholder="fullName" disabled>
@@ -9,19 +9,27 @@
          
             <div class="user-info">
                 <label>Data</label>
-                <Calendar @resetDate="resetDate()" @setDateForAvaliablePlace="setCalendarDate"/>
+                <Calendar 
+                    :date="date" 
+                    @setDateForAvaliablePlace="setCalendarDate"
+                    @resetDate="resetDate()"
+                    :warn="calendarWarn" 
+                    @resetWarn="calendarWarn.msg = ''"   
+                />
             </div>
 
-            
             <div class="user-info">
                 <label>Posto</label>
-                <PlaceList :date="date"/>
+                <PlaceList 
+                    :warn="placeListWarn"
+                    :placeProp="place" 
+                    :date="date"
+                    @setPlace="setPlace"
+                />
                 <p>o</p>
-                <button @click="goToBooking()"> <img src="/src/assets/map.svg" alt=""> Usa mappa</button>
+                <button @click="goToBooking()"> <img src="/src/assets/map.svg" alt="">Usa mappa</button>
             </div>
                  
-  
-
             <SeatCounter @addChair="addChair" @removeChair="removeChair"/>
             
             <div class="total">
@@ -29,7 +37,12 @@
                 <label id="value">€{{ (price + (this.chair * 5) - 5 ) }}</label>
             </div>
 
-           <input type="button" value="Conferma" @click="book" >
+           <input type="button" value="Conferma" @click="book()" >
+        </div>
+        <div id="success-modal" v-show="operationCompleated">
+            <h1 id="success">Prenotazione effettuata</h1>
+            <p id="success-p" >La prenotazione è stata confermata, per ogni necessità è recuperabile nella sezione <i>"Prenotaizoni effettuate"</i> del menù principale</p>
+            <button @click="closeModal">OK</button>
         </div>
     </div>
 </template>
@@ -39,29 +52,47 @@
     import SeatCounter from '../SeatCounter.vue';    
     import Calendar from './Calendar.vue';
     import PlaceList from './PlaceList.vue';
-    import { useRouter } from 'vue-router'
-
- 
-
+    import { useUserStore } from '../../../domain/user/userStore';
+    import { useRouter } from 'vue-router';
+    import { PlaceRepository } from '../../../data/repository/place';
+    import { AxiosError } from 'axios';
 
     export default {
         name: 'ReservationModal',
-        emits: ["closeModal"],
+        emits: ["toggleReservationModal"],
         props: {
-            "fullName" : String,
+            placeProp: String,
+            dateProp : Date,
         },
         
         data(){
             return {
                 price : 10, 
                 chair: 1,
-                date : null
+                date : null,
+                place : null,
+                fullName: "",
+                calendarWarn: { msg: "" },
+                placeListWarn: { msg: "" }, 
+                operationCompleated: false
             }
         },
 
         setup(){
             const router = useRouter();
-            return {router};
+            const userStore = useUserStore();
+            return { router, userStore };
+        },
+
+        mounted(){
+            if(this.dateProp && this.dateProp.toString() !== "Invalid Date"){
+                this.date = this.dateProp;
+                
+                if(this.placeProp){
+                    this.place = this.placeProp;
+                }
+            }
+            this.fullName = `${this.userStore.getUser.name} ${this.userStore.getUser.surname}`;
         },
 
         components: {
@@ -81,24 +112,72 @@
 
             resetDate(){
                 this.date = null;
+                this.calendarWarn = { msg: "" };
             },
 
             setCalendarDate(date) {
                 this.date = date;
+                this.calendarWarn = { msg: "" };
             },
 
-            book() {
-                let clickedPlace = document.getElementsByClassName("ombrellone")[parseInt(this.place-1)];
-                clickedPlace.style.backgroundColor = "red";
+            setPlace(place) {
+                this.place = place;
+                this.placeListWarn = { msg: "" };
+            }, 
+
+            async closeModal(){       
+                this.$emit("toggleReservationModal");
+                this.router.replace({name: "LoggedInDefault"});  
+            },
+
+            async book() {
+                if(this.place === undefined){
+                    this.placeListWarn = { msg: "Selziona un posto" };
+                }
+                
+                if(this.date === null){
+                    this.calendarWarn = { msg: "Seleziona una data" };
+                } 
+
+                if(this.placeListWarn.msg !== '' || this.calendarWarn.msg !== ''){
+                    return;
+                }
+
+                let operation = await PlaceRepository.makeReservarions(this.date, this.place, this.chair);
+
+                if(operation instanceof AxiosError){
+                    console.log("axios error");
+                    return;
+                } else {
+                    this.operationCompleated = true;
+                    return;
+                }
             },
 
             async goToBooking()  {   
-                console.log(this.date);
-                console.log(Math.floor(this.date.getTime() / 1000));
-                await this.router.push({
-                    name: "Booking",
-                    params: { date: `${ this.date.getTime() }`,  }
-                });
+                
+                if(this.dateProp && this.dateProp.toString() === "Invalid Date"){
+                    if(this.date === null){
+                        this.calendarWarn = { msg: "Seleziona una data" };
+                        return;
+                    }
+                }
+                
+                if(this.date !== null) {
+                    console.log("PUSH WITH DATE")
+
+                    await this.router.push({
+                        name: "Booking",
+                        params: { date: `${this.date.getTime()}` }
+                    });
+                } else {
+
+                    console.log("PUSH WITH DATEPROP")
+                    await this.router.push({
+                        name: "Booking",
+                        params: { date: `${this.dateProp.getTime()}` }
+                    });
+                }
             }
         }
     };
@@ -109,6 +188,31 @@
 
     input[type=text]:disabled{
         background-color: gainsboro;
+    }
+
+    #success {
+        font-size: 26px;
+        margin: 0;
+    }
+
+    #success-p{
+        min-width: 300px;
+    }
+
+    #success-modal{
+        background-color: white;
+        z-index: 3;
+        padding: 30px 10px;
+        border-radius: 5px;
+        display: flex;
+        flex-direction: column;
+        margin: 0 30vw;
+        min-width: 300px;
+    }
+
+    #success-modal > button {
+        width: 275px;
+        margin-top: 10px;
     }
 
     button {
@@ -135,6 +239,7 @@
     .inptutLabel {
         font-size: 16px;
     }
+
     #modal-bg {
         display: flex;
         z-index: 2;
@@ -188,5 +293,6 @@
         line-height: 8vw;
         text-align: center;
         align-self: end;
+        cursor: pointer;
     }
 </style>
