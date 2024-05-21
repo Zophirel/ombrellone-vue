9164bb3799da31bb113aedf17ee4b2a212f92912
Francesco <sworld.design@gmail.com>
2024-05-21 16:43:13 +0200

    added warning feature
    refractored li dynamic class, and edited computed function for providing place data in the loop

--- src/presentation/components/reservation/PlaceList.vue
+++ src/presentation/components/reservation/PlaceList.vue
@@ -1,17 +1,18 @@
 <template>
     <div id="authModalCtn">
-        <input :disabled="$props.date === null" :value="setPlaceInputValue" type="text" @click="openList" >
+        <input :class="{warn : warn.msg != ''}" :disabled="$props.date === null" :value="getPlace" type="text" @click="openList" >
+        <p id="warnText" v-show="warn.msg != '' ">{{ warn.msg }}</p>
         <Transition name="placeList" > 
             <div class="placeListWrapper">
-                <div class="placeList" v-show="isListOpen && $props.date !== null">
+                <div class="placeList" v-show="isListOpen && date !== null">
                     <ul>
                         <li 
                         @click = "clickedLi(place)"
                         :class=" 
                             { 
-                                listLabel: place.index === undefined,  
-                                notBooked: place.index === undefined && date !== null ? false : $props.date === null ? false : !place.reservations.includes(date.toISOString()), 
-                                booked: place.index === undefined && date !== null ?  false : $props.date === null ? false : place.reservations.includes(date.toISOString()),
+                                listLabel: isListLabel(place),  
+                                notBooked: isNotBooked(place), 
+                                booked: isBooked(place),
                             }" 
                         v-for="place in places"> {{ place.index === undefined ? place : `${place.index}${place.row}`}} </li>
                     </ul>
@@ -29,9 +30,11 @@
 
     export default {
         name: 'PlaceList',
-
+        emits: ['setPlace'],
         props: {
-            date: Date
+            warn: Object,
+            date: Date,
+            placeProp: String
         },
 
         data(){
@@ -44,12 +47,24 @@
         },
 
         async mounted(){
-           
+  
+            console.log("PLACE LIST MOUNT")
+            console.log(this.date);
             // Get all the data regarding the avaliable places
             if(this.placeStore.getBookingPerPlace.length === 0){
                 this.placeStore.setBookingPerPlaceList(await PlaceRepository.getAllReservationsPerPlace()); 
             }
+
             this.places = this.placeStore.getBookingPerPlace;
+        
+            if(this.placeProp !== null){
+                this.selectedPlace = this.placeProp;
+                this.getPlace;
+            }
+
+            if(this.date !== null){
+                this.lastDateSelected = this.date
+            }
         },
         
         
@@ -59,16 +74,30 @@
         },
 
         computed: {
-            setPlaceInputValue(){
-                const { selectedPlace } = this;
 
-                if(this.$props.date === null){
-                    return "";
-                } else if(this.$props.date !== null && this.lastDateSelected !== this.$props.date){
-                    this.lastDateSelected = this.$props.date;
-                    return "";
-                } 
-                return selectedPlace;
+            getPlace(){
+                const { selectedPlace } = this;
+                if(this.placeProp === undefined && this.selectedPlace === null){
+                    return '';
+                }  else if(this.placeProp !== undefined && this.selectedPlace === null){
+                    return this.placeProp;
+                } else {
+                    let elementFound = this.places.find((elem) => {                        
+                       if(elem.index && elem.reservations){
+                            return `${elem.index}${elem.row}` === this.selectedPlace 
+                                && elem.reservations.includes(this.date.toISOString())
+                        }
+                           
+                    });
+                    // reset the input
+                    if(elementFound !== undefined){
+                        console.log("PLACE LIST RESET INPUT")
+                        this.$emit('setPlace', undefined);
+                        return "";
+                    } else {
+                        return selectedPlace;
+                    }
+                }
             }
         },
 
@@ -82,21 +111,50 @@
             },
             
             clickedLi(place) {
-               
                 // If place tile has been clicked it will have an index prop
                 // if label tile has been clicked it won't
-                if(place.index !== undefined){
+                if(!this.isListLabel(place) && this.isNotBooked(place)){
                     if( this.selectedPlace != `${place.index}${place.row}`){
                         this.selectedPlace = `${place.index}${place.row}`;
                     }else{
-
                         // Refresh the input value when the user 
                         // select the same place in two different dates
-                        this.setPlaceInputValue;
+                        this.getPlace;
                     }
+                    this.$emit('setPlace', this.selectedPlace);
+                    
                     this.isListOpen = false;                
                 } 
             },
+
+            isListLabel(place){
+                if(place.index){
+                    return false;
+                }
+                return true
+            },
+            
+            isBooked(place){
+                if(!this.isListLabel(place)){
+                    if(this.date !== null && place.reservations.includes(this.date.toISOString())){
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+                return false;
+            },
+            
+            isNotBooked(place){
+                if(!this.isListLabel(place)){
+                    if(this.date !== null && !place.reservations.includes(this.date.toISOString())){
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+                return false;
+            }
         }
     };
 </script>
@@ -109,6 +167,21 @@ input[type=text]{
     border-radius: 5px;
     margin-bottom: 0px;
     padding: 0 15px;
+    margin-bottom: 10px;
+    
+}
+
+input[type=text].warn {
+        border: 2px solid red;
+    }
+
+#warnText {
+    margin: 0;
+    position: relative;
+    bottom: 6px;
+    font-size: 14px;
+    color: red;
+    text-align: start;
 }
 
 .placeList {
