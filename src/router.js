200962c66a24823de616a88f2777c2e3929aeae1
Francesco <sworld.design@gmail.com>
2024-05-27 15:36:26 +0200

    added and edited some routes path
    edited HomeDefault and added transition
    
    added BookingInfo route
    
    replaced LoggedInData route with  LoggedInBook and used one path param instead of one for each prop using json stringfy to pass data
    
    added LoggedInShowBooked to open the LoggedIn Booked modal on route pushing

--- src/router.js
+++ src/router.js
@@ -2,44 +2,61 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 import HomePage from './presentation/components/HomePage.vue'
 import BookingPage from './presentation/components/BookingPage.vue'
 import LoggedIn from './presentation/components/LoggedIn.vue'
+import BookedInfo from './presentation/components/BookedInfo.vue'
 
 const routes = [
-    { 
-      name: "HomeDefault",
-      path: '/', 
-      component: HomePage,
-    },
+  { 
+    name: "HomeDefault",
+    path: '/', 
+    component: HomePage,
+    meta: { transition: 'home-out-right' }
+  },
 
-    { 
-      name: "HomeAnimation",
-      path: '/', 
-      component: HomePage,
-      meta: { transition: 'home-in-top' }
-    },
+  { 
+    name: "HomeAnimation",
+    path: '/', 
+    component: HomePage,
+    meta: { transition: 'home-in-top' }
+  },
 
-    {  
-      name: "Booking",
-      path: '/booking/:date', 
-      component: BookingPage,  
-      props: { BookingPage: true, numberOfUmbrella: 150, warn: {msg: ""} },
-      meta: { transition: 'booking-in-bottom' }
-    },
+  {  
+    name: "Booking",
+    path: '/booking/:date', 
+    component: BookingPage,  
+    props: { BookingPage: true, numberOfUmbrella: 150, warn: {msg: ""} },
+    meta: { transition: 'booking-in-bottom' }
+  },
 
-    {  
-      name: "LoggedInDefault",
-      path: '/logged', 
-      component: LoggedIn,  
-      meta: { transition: 'logged-in-right' }
-    },
+  {
+    name: "BookedInfo",
+    path: '/bookedinfo/:bookingJsonData',
+    component: BookedInfo,
+    props: true,
+    meta: { transition: 'logged-in-right' }
+  },
 
-    {  
-      name: "LoggedInData",
-      path: '/logged/:dateProp/:optionProp/:placeProp', 
-      component: LoggedIn,
-      props: true,  
-      meta: { transition: 'home-in-top' }
-    },
+  {  
+    name: "LoggedInDefault",
+    path: '/logged', 
+    component: LoggedIn,  
+    meta: { transition: 'logged-in-right' }
+  },
 
+  {  
+    name: "LoggedInBook",
+    path: '/logged/:bookingData', 
+    component: LoggedIn,
+    props: true,  
+    meta: { transition: 'logged-in-right' }
+  },
+
+  {  
+    name: "LoggedInShowBooked",
+    path: '/logged/', 
+    component: LoggedIn,
+    props: {LoggedIn: true, bookedData: true},  
+    meta: { transition: 'logged-in-right' }
+  }
 ]
 
 const router = createRouter({
